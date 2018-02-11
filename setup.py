fb698b09ee1a888ce7fde2a1a6e204d3feba19ba
nimrod z <nimz911@gmail.com>
2018-02-11 16:06:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Nimrod Zilberstein',
   author_email = 'nimz911@gmail.com',
   url = 'https://github.com/nimz911/Facebook_Data_MiningPY', # use the URL to the github repo
-  download_url = 'https://github.com/nimz911/Facebook_Data_MiningPY/archive/Facebook_Data_MiningPY_1.0.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/nimz911/Facebook_Data_MiningPY/archive/1.0.tar.gz', # I'll explain this in a second
   keywords = ['facebook', 'data mining'], # arbitrary keywords
   classifiers = ['Programming Language :: Pyhton :: 2','Programming Language :: Pyhton :: 2.7'],
 )
