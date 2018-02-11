7caf9fe3161b0e1ce326cc4b211ff1c37be2d44f
nimrod z <nimz911@gmail.com>
2018-02-11 16:33:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'Facebook_Data_MiningPY',
   packages = ['Facebook_Data_MiningPY'], # this must be the same as the name above
-  version = '1.0',
+  version = '1.2.0',
   description = 'read Facebook groups or pages data',
   author = 'Nimrod Zilberstein',
   author_email = 'nimz911@gmail.com',
@@ -10,5 +10,7 @@ setup(
   download_url = 'https://github.com/nimz911/Facebook_Data_MiningPY/archive/1.0.tar.gz', # I'll explain this in a second
   keywords = ['facebook', 'data mining'], # arbitrary keywords
   license='MIT',
-  classifiers = ['Programming Language :: Pyhton :: 2','Programming Language :: Pyhton :: 2.7'],
+  install_requires=['pandas','nltk','facebook-sdk','requests','time','datetime','json'],
+  classifiers = ['Programming Language :: Pyhton :: 2',
+                 'Programming Language :: Pyhton :: 2.7'],
 )
